5ecc2a4e92f91024ee9e354f1899fe9e71c1c440
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-18 21:46:00 -0500

    Added the Home component to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'contact',
     component: ContactComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, ContactComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ContactComponent,
+    HomeComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
