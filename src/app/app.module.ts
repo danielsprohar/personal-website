10b366ab057dae84c946b22959c3d1cffc666209
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-19 22:02:11 -0500

    Added firebase firestore config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,14 @@ import { HomeComponent } from './components/home/home.component';
     ContactComponent,
     HomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, RouterModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'sprohar-dev',
+    appId: '1:210095279703:web:1a4b3aff0e93dc0dee1d12',
+    storageBucket: 'sprohar-dev.appspot.com',
+    apiKey: 'AIzaSyAowlm26S2QmKRXb7-yhkz1vs8NRhzaRqE',
+    authDomain: 'sprohar-dev.firebaseapp.com',
+    messagingSenderId: '210095279703',
+  },
+};
