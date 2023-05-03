2b82dee323f3589777f609d99b44ea77e475d4d4
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-02 22:07:56 -0500

    Modified the Navbar component to use Bootstrap

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,4 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -9,12 +7,5 @@ import { filter, map } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent implements OnInit {
-  url$ = this.router.events.pipe(
-    filter((event) => event instanceof NavigationEnd),
-    map((event) => (event as NavigationEnd).urlAfterRedirects)
-  );
-
-  constructor(private router: Router) {}
-
   ngOnInit(): void {}
 }
