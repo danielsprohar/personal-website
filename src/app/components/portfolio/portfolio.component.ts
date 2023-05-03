71abd277eedf74d50747105d7ffdc1f370c15af6
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-02 23:40:07 -0500

    Cleaned up the portfolio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PortfolioComponent {
