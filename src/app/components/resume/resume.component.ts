9ec27d5a66f6b9a35362e432d59148faa991034c
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-17 17:33:39 -0500

    Resume: added the cert

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
+  styleUrls: ['./resume.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResumeComponent {}
