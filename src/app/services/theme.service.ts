4d1f68308359078a5172b6743370876eb163574d
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-25 12:23:12 -0500

    Added a Theme service

---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, fromEvent, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private readonly _isDarkTheme$ = new BehaviorSubject<boolean>(true);
+  readonly isDarkTheme$ = this._isDarkTheme$.asObservable();
+
+  readonly mql$ = fromEvent(
+    window.matchMedia('(prefers-color-scheme: dark)'),
+    'change'
+  ).pipe(map((event: Event) => event.target as MediaQueryList));
+
+  constructor() {}
+
+  toggle() {
+    this._isDarkTheme$.next(!this._isDarkTheme$.value);
+  }
+
+  setLightTheme() {
+    this._isDarkTheme$.next(false)
+  }
+
+  setDarkTheme() {
+    this._isDarkTheme$.next(true)
+  }
+}
